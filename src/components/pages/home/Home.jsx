663cdc5c3e827563d6d97469903ed99c7b16cd08
Valk <bznand@gmail.com>
2024-01-22 16:05:38 +0100

    completata struttura base progetto

---
+++ src/components/pages/home/Home.jsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <>
+      <p>ciao sono home</p>
+      <Link to="/profile/pepe">vai al profilo</Link>
+    </>
+  );
+}
+
+export default Home;
