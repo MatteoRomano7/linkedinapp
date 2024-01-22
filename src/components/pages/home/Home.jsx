bf9afbf9557295bb2ac9210d6e5dd931af10fa3b
gabrieledelfrate <leledelfrate@outlook.com>
2024-01-23 00:49:27 +0100

    creato language.jsx hiring.jsx importati in home e profile

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,10 +1,13 @@
 import { Link } from "react-router-dom";
+import Hiring from "../hiring/Hiring";
 
 function Home() {
   return (
     <>
       <p>ciao sono home</p>
       <Link to="/profile/pepe">vai al profilo</Link>
+      
+      <Hiring />
     </>
   );
 }
