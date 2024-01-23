18a55a761d385146d16beff07bedfe388824354a
allenn <marco.guerrieri@live.it>
2024-01-23 15:00:14 +0100

    template completo profilo iniziallizzazione della Home

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,10 +1,12 @@
 import { Link } from "react-router-dom";
+import LinkedinNews from "./LinkedinNews";
 
 function Home() {
   return (
     <>
       <p>ciao sono home</p>
       <Link to="/profile/pepe">vai al profilo</Link>
+      <LinkedinNews />
     </>
   );
 }
---
+++ src/components/pages/home/LinkedinNews.jsx
@@ -0,0 +1,37 @@
+import { InfoSquareFill } from "react-bootstrap-icons";
+import { ArrowDown } from "react-bootstrap-icons";
+import { Link } from "react-bootstrap-icons";
+import img from "../../../../public/assets/AAYQAgTPAAgAAQAAAAAAADVuOvKzTF-3RD6j-qFPqhubBQ.png"
+
+const LinkedinNews = () => {
+    return (
+        <div>
+            <div className="d-flex justify-content-between">
+                <h2>LinkedIn Notizie</h2>
+                <InfoSquareFill />
+            </div>
+            <div>
+                <ul className="m-2">
+                    <li>I 15 lavori in piú rapida crescita in italia</li>
+                    <span>Notizi principali </span>
+                    <li>Cercare lavoro nella tecnologia</li>
+                    <span> 20 ore fa</span>
+                    <li>SONDAGGIO: cosa valuti di piú in un'offerta?</li>
+                    <span>2 ore fa</span>
+                    <li>Un pomodoro per lo spazio</li>
+                    <span>3 giorni fa</span>
+                    <li>Le cittá dei lavori in crescita</li>
+                    <span>1 giorno fa</span>
+                </ul>
+            </div>
+            <div className="d-flex justify-content-center">
+                <h2>Show more</h2> <ArrowDown className="aling-items-center m-2" />
+            </div>
+            
+            <Link to='/Job' />
+
+        </div>
+    )
+};
+
+export default LinkedinNews;
