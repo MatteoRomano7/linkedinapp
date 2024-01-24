9c1456662037aa44c415e9cb154ef0641cd9d215
Matteo <ezmatteo@aol.com>
2024-01-24 23:48:06 +0100

    fix Mini Footer

--- src/components/MiniFooter/MiniFooter.jsx
+++ src/components/MiniFooter/MiniFooter.jsx
@@ -1,13 +1,11 @@
-import React from "react";
-import "bootstrap/dist/css/bootstrap.min.css";
-import { Container, Dropdown } from 'react-bootstrap';
-import './MiniFooter.css'
-
+import React from "react"
+import "bootstrap/dist/css/bootstrap.min.css"
+import { Container, Dropdown } from "react-bootstrap"
+import "./MiniFooter.css"
 
 const MiniFooter = () => {
   return (
     <>
-    
       <div className="d-flex  justify-content-center">
         <div className="mx-2">Informazioni</div>
         <div className="">Accessibilità</div>
@@ -17,7 +15,11 @@ const MiniFooter = () => {
         <div className="mt-2">Centro assistenza</div>
         <div className="">
           <Dropdown>
-            <Dropdown.Toggle variant="white" id="privacyDropdown2" className="btn-white">
+            <Dropdown.Toggle
+              variant="white"
+              id="privacyDropdown2"
+              className="btn-white"
+            >
               Privacy e condizioni
             </Dropdown.Toggle>
             <Dropdown.Menu className="bingo">
@@ -29,16 +31,18 @@ const MiniFooter = () => {
       </div>
 
       <div className="d-flex justify-content-center">
-        <div className="">
-          Opzioni per gli annunci
-        </div>
+        <div className="">Opzioni per gli annunci</div>
       </div>
 
       <div className="d-flex justify-content-center">
         <div className="mt-2">Pubblicità</div>
         <div className="">
           <Dropdown>
-            <Dropdown.Toggle variant="white" id="privacyDropdown2" className="bingo">
+            <Dropdown.Toggle
+              variant="white"
+              id="privacyDropdown2"
+              className="bingo"
+            >
               Servizi alle aziende
             </Dropdown.Toggle>
             <Dropdown.Menu className="bingo">
@@ -54,13 +58,16 @@ const MiniFooter = () => {
         <div className="">Altro</div>
       </div>
 
-      <div className="d-flex justify-content-center mt-3 ">
-        <div className=""><img src={"img/linkedin_logo_footer.png"} alt="LinkedIn Logo" /></div>
-        <div className="FooterGrassseto mt-1">LinkedIn Corporation &copy; 2024</div>
+      <div className="d-flex justify-content-center mt-3 signatureLogo ">
+        <div className="">
+          <img src={"img/linkedin_logo_footer.png"} alt="LinkedIn Logo" />
+        </div>
+        <div className="FooterGrassseto mt-1">
+          LinkedIn Corporation &copy; 2024
+        </div>
       </div>
-     
     </>
-  );
-};
+  )
+}
 
-export default MiniFooter;
+export default MiniFooter
